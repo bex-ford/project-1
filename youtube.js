92b18fc1a3c8641db4f84160d60d265358e19516
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-02-02 21:57:42 +0000

    youtube code API request & styling

--- youtube.js
+++ youtube.js
@@ -1,15 +1,17 @@
-console.log("Hello!");
-
 fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q=gluten%20free%20vegetarian%20recipes&key=AIzaSyBtVcpEBC7OOCsQxa7h_5TCjeUuaynHiWo")
-.then((result) => {
-    return(result.json())
-}).then((data)=> {
-    console.log(data);
+.then((result)=>{
+    return result.json()
+}).then((data)=>{
+    console.log(data)
     let videos = data.items
     let videoContainer = document.querySelector(".youtube-container")
     for(video of videos){
         videoContainer.innerHTML += `
-        <img src="${video.snippet.thumbnails.default.high.url}">
+        <h3 class= "video-title">${video.snippet.title}</h3>
+        <br>
+        <img class="video-img" src="${video.snippet.thumbnails.medium.url}">
+        <br>
+        <a href="https://www.youtube.com/watch?v=${video.snippet.videoId}" class="btn btn-danger video-btn">Watch on YouTube</a>
         `
     }
 });    
