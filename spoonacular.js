85e8a378fe6cf1296219a64fe5ca23f93fc5caf9
bex-ford <becky782@hotmail.co.uk>
2023-02-05 17:03:27 +0000

    checked API key working

--- spoonacular.js
+++ spoonacular.js
@@ -1,12 +1,15 @@
-let apiKey = "cff15fd5c68a4190b152f91d1c8785a0"; // API Key
-let recipeName = $("#dish").val();
-let cuisineInput = $("#cuisine").val();
-let includedIngredients = $("#include").val();
-let excludedIngredients = $("#exclude").val();
-let searchBtn = $("#search-button");
+var apiKey = "cff15fd5c68a4190b152f91d1c8785a0"; // API Key
+var recipeName = $("#dish").val();
+console.log(recipeName)
+var cuisineInput = $("#cuisine").val();
+var includedIngredients = $("#include").val();
+var excludedIngredients = $("#exclude").val();
+const searchBtn = $("#search-button");
+const recipeList = $("#recipe");
 
 function spoonacularQuery() {
-  let queryUrl =
+  var recipeName = $("#dish").val();
+  var queryUrl =
     "https://api.spoonacular.com/recipes/complexSearch?apiKey=" +
     apiKey +
     "&diet=glutenfree,vegetarian";
@@ -30,40 +33,41 @@ function spoonacularQuery() {
     method: "GET",
     url: queryUrl,
   }).done(function (response) {
-    $("dish").text(JSON.stringify(response));
+    // $("dish").text(JSON.stringify(response));
     console.log(response);
   });
 }
 searchBtn.click(spoonacularQuery);
 
+
 // fetch API
-function getRecipeName(){
-  let searchInputTxt = document.getElementById
-  ("dish").value.trim();
+function getRecipeList() {
+  let searchInputTxt = document.getElementById("dish").value.trim();
   console.log(searchInputTxt);
-  fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cff15fd5c68a4190b152f91d1c8785a0=${searchInputTxt}`)
-  .then(response => response.json())
-  .then(data => {
-    let html = "";
-    if(data.recipeName){
-      data.recipeName.forEach(recipeName => {
-        html += `
-          <div class = "recipe-item" data-id = "${recipeName.idRecipe}">
-          <div class = "recipe-img">
-          <img src = "${recipeName.strRecipeThumb}" alt = "food">
-          </div>
-          <div class = "recipe-name">
-          <h3>${recipeName.strRecipe}</h3>
-          <a href = "#" class = "recipe-btn">Get Recipe</a>
-          </div>
-          </div>
+  fetch( 
+  `https://api.spoonacular.com/recipes/complexSearch?apiKey=cff15fd5c68a4190b152f91d1c8785a0&diet=glutenfree,vegetarian&cuisine=italian&includeIngredients=&excludeIngredients=`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let html = "";
+      if (data.recipe) {
+        data.recipe.forEach((recipe) => {
+          html += `
+        <div class = "recipe-item" data-id ="${results.title}">
+        <div class = "recipe-img">
+          <img src = "${results.image}" alt = "food">
+        </div>
+        // <a href = "#" class = "recipe-btn">Get Recipe</a>
+        </div>
+      </div>
+       
         `;
-      });
-    } else {
-      html = "Sorry, we didn't find your recipe!"
-    }
+        });
+      } else {
+        html = "Sorry, we didn't find your recipe!";
+      }
 
-  })
-  getRecipeName.innerHTML = html;
+    });
+document.getElementById("getRecipeList").innerHTML = html;
 }
-
+getRecipeList()
