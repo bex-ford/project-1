57fd3912e0255c0f084239dc1781c07448475c86
bex-ford <becky782@hotmail.co.uk>
2023-02-05 09:59:51 +0000

    changes to js

--- spoonacular.js
+++ spoonacular.js
@@ -5,9 +5,6 @@ let includedIngredients = $("#include").val();
 let excludedIngredients = $("#exclude").val();
 let searchBtn = $("#search-button");
 
-// event listeners 
-searchBtn.addEventListener('click', getRecipes);
-
 function spoonacularQuery() {
   let queryUrl =
     "https://api.spoonacular.com/recipes/complexSearch?apiKey=" +
@@ -33,54 +30,40 @@ function spoonacularQuery() {
     method: "GET",
     url: queryUrl,
   }).done(function (response) {
+    $("dish").text(JSON.stringify(response));
     console.log(response);
   });
 }
 searchBtn.click(spoonacularQuery);
 
 // fetch API
-function getRecipes(){
+function getRecipeName(){
   let searchInputTxt = document.getElementById
-  ('search-input').value.trim();
+  ("dish").value.trim();
+  console.log(searchInputTxt);
   fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cff15fd5c68a4190b152f91d1c8785a0=${searchInputTxt}`)
   .then(response => response.json())
   .then(data => {
     let html = "";
-    if(data.meals){
-      data.meals.forEach(meal => {
+    if(data.recipeName){
+      data.recipeName.forEach(recipeName => {
         html += `
-          <div class = "meal-item" data-id = "${meal.idMeal}">
-          <div class = "meal-img">
-          <img src = "${meal.strMealThumb}" alt = "food">
+          <div class = "recipe-item" data-id = "${recipeName.idRecipe}">
+          <div class = "recipe-img">
+          <img src = "${recipeName.strRecipeThumb}" alt = "food">
           </div>
-          <div class = "meal-name">
-          <h3>${meal.strMeal}</h3>
+          <div class = "recipe-name">
+          <h3>${recipeName.strRecipe}</h3>
           <a href = "#" class = "recipe-btn">Get Recipe</a>
           </div>
           </div>
         `;
       });
     } else {
-      html = "Sorry, we didn't find any meal!"
+      html = "Sorry, we didn't find your recipe!"
     }
 
   })
-  getRecipes.innerHTML = html;
+  getRecipeName.innerHTML = html;
 }
-
-
-
-//   method:'GET',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     recipe: 1
-//   })
-// }).then(res => {
-//   return res.json()
-// })
-//   .then(data => console.log(data))
-//   .catch(error => console.log("ERROR"))
-// let recipeIdeas = document.querySelector("#recipe");
 
